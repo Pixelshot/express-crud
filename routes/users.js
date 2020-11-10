cd52a1b4f4fb4e8370940a13838891db68a41358
Pixelshot <hazrijordan@gmail.com>
2020-11-10 15:59:27 +0800

    Route for specific id done

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,12 @@
 import express from 'express';
 
+// Use npm package uuid to create unique id for users
+import { v4 as uuidv4 } from 'uuid';
+
 // Using express's Router function for our get request
 const router = express.Router();
 
-const users = [
-  {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 25,
-  },
-  {
-    firstName: 'Jane',
-    lastName: 'Doe',
-    age: 24,
-  },
-];
+const users = [];
 
 // All routes in here are already starting with '/users'. This setup can be found in index.js
 router.get('/', (req, res) => {
@@ -30,13 +22,26 @@ router.post('/', (req, res) => {
 
   // Adding new user to the database. In our example it's just a simple array(users)
   const user = req.body;
-  users.push(user);
+
+  // Insert id to user
+  users.push({ ...user, id: uuidv4() });
 
   console.log(users);
   // What client side will see
   res.send(
     `User with the name ${user.firstName} has been added to the database!`
   );
+});
+
+// To retrieve a specific user, we use params
+// Eg: /users/2 => req.params { id: 2 }
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  // How to grab the specific user from our database
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
 });
 
 export default router;
