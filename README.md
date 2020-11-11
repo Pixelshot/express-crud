01f0df6a8744eb1becd56ec2eb1cf878da302d91
Pixelshot <hazrijordan@gmail.com>
2020-11-11 11:11:06 +0800

    Created a default database & completed Patch

---
+++ README.md
@@ -0,0 +1,3 @@
+[A good explanation on what REST API is](https://www.smashingmagazine.com/2018/01/understanding-using-rest-api/)
+
+[Link to the REST API tutorial using Node & Express](https://www.youtube.com/watch?v=l8WPWK9mS5M&t=2671s&ab_channel=JavaScriptMastery)
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,26 @@ import { v4 as uuidv4 } from 'uuid';
 // Using express's Router function for our get request
 const router = express.Router();
 
-let users = [];
+let users = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    id: uuidv4(),
+  },
+  {
+    firstName: 'Barry',
+    lastName: 'Allen',
+    age: 32,
+    id: uuidv4(),
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 22,
+    id: uuidv4(),
+  },
+];
 
 // All routes in here are already starting with '/users'. This setup can be found in index.js
 router.get('/', (req, res) => {
@@ -42,6 +61,34 @@ router.get('/:id', (req, res) => {
   // How to grab the specific user from our database
   const foundUser = users.find((user) => user.id === id);
   res.send(foundUser);
+});
+
+// There are two ways to edit/update a user
+// 1. PUT - This will completely override data of the user
+// 2. PATCH - To apply partial modification
+
+// 2. PATCH
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+
+  // These are coming from Postman where we manually update the user
+  const { firstName, lastName, age } = req.body;
+
+  // Getting the specific user using id
+  const user = users.find((user) => user.id === id);
+
+  // Using if statement to insert the new updates
+  // Normal syntax
+  // if (firstName) {
+  //   user.firstName = firstName;
+  // }
+
+  // Shorthand syntax
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (age) user.age = age;
+
+  res.send(`User with id ${id} has been updated`);
 });
 
 // Deleting a specific user
